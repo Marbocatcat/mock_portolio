06e4c8b0bad2996ae5b6a4ee44353f2e4785866e
Marbocatcat <mar.bocatcat@gmail.com>
2017-04-30 15:14:08 -0500

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import IntroArticle from './Article_Intro/IntroArticle';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <IntroArticle />
+      </div>
+
+    )
+  }
+};
---
+++ src/components/Article_Intro/IntroArticle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import SubHeader from './SubHeader';
+
+const IntroArticle = (props) => {
+  return (
+    <article>
+      <Header />
+      <SubHeader />
+    </article>
+  )
+}
+
+export default IntroArticle;
